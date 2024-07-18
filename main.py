7bff59bf5b719152edba63ca7dbd5de6e33d316c
Shantanu218 <shantanu18206@gmail.com>
2024-07-18 12:13:00 +0800

    Added invoice date to PDF

--- main.py
+++ main.py
@@ -10,16 +10,22 @@ print(filepaths)
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name=f"Sheet 1")
+    
     pdf = FPDF(orientation = "P", unit = "mm", format = "A4")
     pdf.add_page()
+    
     filename = Path(filepath).stem
-    invoice_number = filename.split("-")[0]
+    invoice_number, date = filename.split("-")
+    
     pdf.set_font(family = "Times", size = 16, style = "B")
-    pdf.cell(w=50, h=8, txt = f"Invoice number: {invoice_number}")
+    pdf.cell(w=50, h=8, txt = f"Invoice number: {invoice_number}", ln=1)
+
+    pdf.set_font(family = "Times", size = 16, style = "B")
+    pdf.cell(w=50, h=8, txt = f"Invoice number: {date}")
+
     pdf.output(f"PDFs/{filename}.pdf")
     print(df)
-
-
+    
 # Press Ctrl+F5 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
