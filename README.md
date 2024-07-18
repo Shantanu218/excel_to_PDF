1b75838f66ccbc884a60ddca4eee690097046bd1
Shantanu218 <shantanu18206@gmail.com>
2024-07-18 16:10:05 +0800

    Added the header row and the data rows. Also added the total price and company logo

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # What is this app?
 
 This is an app that creates PDF invoices out of excel sheets
+
+Add your file to the invoices folder to use this app
--- main.py
+++ main.py
@@ -4,8 +4,30 @@ import glob
 from fpdf import FPDF
 from pathlib import Path
 
-filepaths = glob.glob("invoices/*.xlsx")
 
+# Code to make a new cell
+def create_cell(arg):
+	pdf.cell(w = 40, h = 8, txt = str(arg).replace("_", " ").title(), border = 1)
+
+
+# Code to make a new end cell
+def create_end_cell(arg):
+	pdf.cell(w = 40, h = 8, txt = str(arg).replace("_", " ").title(), border = 1, ln = 1)
+
+
+# Code to make an entire row when given a list of values as an input
+def create_all_cells(args):
+	for index, item in enumerate(args):
+		if index + 1 == len(args):
+			break
+		create_cell(item)
+	create_end_cell(args[-1])
+
+
+rowLayout = ["product_id", "product_name", "amount_purchased",
+			 "price_per_unit", "total_price"]
+
+filepaths = glob.glob("invoices/*.xlsx")
 for filepath in filepaths:
 	pdf = FPDF(orientation = "P", unit = "mm", format = "A4")
 	pdf.add_page()
@@ -19,39 +41,29 @@ for filepath in filepaths:
 	pdf.set_font(family = "Times", size = 16, style = "B")
 	pdf.cell(w = 50, h = 8, txt = f"Invoice date: {date}", ln = 1)
 	
-	rowLayout = ["product_id", "product_name", "amount_purchased",
-				 "price_per_unit", "total_price"]
-	
-	
-	# Code to make a new cell
-	def create_cell(arg):
-		pdf.cell(w = 40, h = 8, txt = str(arg).replace("_", " ").title(), border = 1)
-	
-	
-	# Code to make a new end cell
-	def create_end_cell(arg):
-		pdf.cell(w = 40, h = 8, txt = str(arg).replace("_", " ").title(), border = 1, ln = 1)
-	
-	
 	pdf.set_font(family = "Times", size = 10, style = "B")
 	pdf.set_text_color(80, 80, 80)
 	
-	# Add a header
-	for item in rowLayout:
-		if item == "total_price":
-			create_end_cell(item)
-		else:
-			create_cell(item)
+	# Makes a header row
+	create_all_cells(rowLayout)
 	
 	dataframe = pd.read_excel(filepath, sheet_name = f"Sheet 1")
-	# Add rows
+	# Makes a row for each product
 	for index, row in dataframe.iterrows():
 		pdf.set_font(family = "Times", size = 10)
-		# Add cells
-		for item in rowLayout:
-			if item == "total_price":
-				create_end_cell(row[item])
-			else:
-				create_cell(row[item])
+		# Adds all the cells for each product
+		create_all_cells(row.tolist())
+	
+	total_sum = dataframe["total_price"].sum()
+	sumLayout = [" "] * (len(rowLayout) - 1)
+	sumLayout.append(str(total_sum))
+	create_all_cells(sumLayout)
+	
+	pdf.set_font(family = "Times", size = 16, style = "B")
+	pdf.cell(w = 50, h = 8, txt = f"The total price for all items: {total_sum}", ln = 1)
+	
+	pdf.set_font(family = "Times", size = 16, style = "B")
+	pdf.cell(w = 30, h = 8, txt = f"PythonHow")
+	pdf.image("pythonhow.png", w = 10)
 	
 	pdf.output(f"PDFs/{filename}.pdf")
