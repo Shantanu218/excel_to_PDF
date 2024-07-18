4c4a8137cd58d21689731a1217725fa94d57fa33
Shantanu218 <shantanu18206@gmail.com>
2024-07-18 11:55:31 +0800

    Extracted Invoice number and added it to the PDF

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 # This is a sample Python script.
 import pandas as pd
 import glob
+from fpdf import FPDF
+from pathlib import Path
+
 
 filepaths = glob.glob("invoices/*.xlsx")
 print(filepaths)
 
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name=f"Sheet 1")
+    pdf = FPDF(orientation = "P", unit = "mm", format = "A4")
+    pdf.add_page()
+    filename = Path(filepath).stem
+    invoice_number = filename.split("-")[0]
+    pdf.set_font(family = "Times", size = 16, style = "B")
+    pdf.cell(w=50, h=8, txt = f"Invoice number: {invoice_number}")
+    pdf.output(f"PDFs/{filename}.pdf")
     print(df)
 
 
